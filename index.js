daab2703d1aa73cb62d05e58b0eb2417e52fac02
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-01 20:37:05 -0400

    get by id with files

--- index.js
+++ index.js
@@ -34,6 +34,26 @@ app.post("/api/v1/products", (req, res) => {
   });
 });
 
+app.get("/api/v1/products/:id", (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/products.json`)
+  );
+
+  const foundProduct = products.find((p) => p.id == req.params.id);
+  if (foundProduct) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        product: foundProduct,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
