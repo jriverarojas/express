0365b933cbe1a16c348ef986acaa26a8cf2c32f7
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-01 20:20:26 -0400

    insert product with files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT;
 
 app.get("/api/v1/products", (req, res) => {
@@ -11,6 +13,21 @@ app.get("/api/v1/products", (req, res) => {
   res.status(200).json({
     status: "success",
     results: products.length,
+    data: {
+      products,
+    },
+  });
+});
+
+app.post("/api/v1/products", (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/products.json`)
+  );
+  products.push(req.body);
+  fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
+
+  res.status(200).json({
+    status: "success",
     data: {
       products,
     },
