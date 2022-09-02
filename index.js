373dbfd824d033874b42f9e9e1c366ae73410402
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-01 21:25:39 -0400

    morgan middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
