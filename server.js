ac5fd2ce2b44e9bfd1a7eb9759069d5abea2af7a
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-06 20:59:56 -0400

    creating a product with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const mongoose = require("mongoose");
+const Product = require("./models/Product");
 const app = require("./app");
 const port = process.env.PORT;
+mongoose.connect(process.env.DATABASE, {}).then((con) => {
+  console.log("Connected to mongo");
+  const p = new Product({ productName: "product 2", price: 10 });
+  p.save().then(() => {
+    console.log("saved");
+  });
+});
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
