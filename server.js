52c0cd70f97553e9da599ed16d79efd3be76f42a
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-08 21:36:31 -0400

    server error management

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
+process.on("uncaughtException", (err) => {
+  console.log("uncaughtException", err);
+  console.log("Shutting down");
+  process.exit(1);
+});
 const Product = require("./models/Product");
 const app = require("./app");
 const port = process.env.PORT;
+
 mongoose.connect(process.env.DATABASE, {}).then((con) => {
   console.log("Connected to mongo");
 });
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("unhandledRejection", err);
+  console.log("Shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
 });
