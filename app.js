77324070e4b1ac433e8f9a8623c3f01456066008
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-08 20:51:39 -0400

    error handling 1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const productRouter = require("./routes/productRoutes");
+const MyError = require("./utils/MyError");
 const app = express();
 
 //middlewares
@@ -13,5 +14,23 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/v1/products/", productRouter);
+
+app.all("*", (req, res, next) => {
+  next(new MyError("route not found", 404));
+});
+
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: "Ocurrio un error",
+    });
+  }
+});
 
 module.exports = app;
---
+++ utils/MyError.js
@@ -0,0 +1,9 @@
+class MyError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+  }
+}
+module.exports = MyError;
